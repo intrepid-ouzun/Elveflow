e7a08938a2f29675942166b3e0557ab7ffbaef4a
intrepid-ouzun <oyku@intrepidlabs.ai>
2025-09-12 16:12:22 -0400

    square waveform PID control

--- calibration.py
+++ calibration.py
@@ -5,6 +5,11 @@ import json
 
 from ctypes import *
 from pathlib import Path
+from collections import deque   # <-- add this line
+import numpy as np              # <-- add this line
+import pandas as pd             # <-- add this line 
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 
 sys.path.append('C:/Users/oykuz/ESI_V3_10_02/SDK_V3_10_01/SDK_V3_10_01/DLL/DLL64')#add the path to Elveflow64.lib here
@@ -16,21 +21,23 @@ from Elveflow64 import *
 channel_MFS = c_int32(2)
 Instr_ID=c_int32(-1) # handle for the SDK communication, increments with each new instrument initialized
 error = OB1_Initialization('113433'.encode('ascii'),0,0,0,0,byref(Instr_ID)) 
-error = OB1_Add_Sens(Instr_ID.value, channel_MFS, 5, 1, 1, 7, 0) 
+error = OB1_Add_Sens(Instr_ID.value, channel_MFS.value, 5, 1, 1, 7, 0) 
 print("added Sensor with error", error)
 
 # ----- CALIBRATION FILE SETUP -----
-Calib_path = r"C:\Users\oykuz\calibration.txt"
+#Calib_path = r"C:\Users\oykuz\calibration.calib"
+Calib_path = r"C:\Users\oykuz\calibration_20250912.CALIB"
 
-# time stamp the calib file
-timestamp = time.strftime("%Y%m%d")
-folder, filename = os.path.split(Calib_path)
-name, ext = os.path.splitext(filename)
 
-# Create new filename with timestamp
-new_filename = f"{name}_{timestamp}{ext}"
-new_path = os.path.join(folder, new_filename)
-path_buf = create_string_buffer(new_path.encode('ascii'))  # char path[] (array), NUL added automatically
+#time stamp the calib file
+# timestamp = time.strftime("%Y%m%d")
+# folder, filename = os.path.split(Calib_path)
+# name, ext = os.path.splitext(filename)
+
+# # Create new filename with timestamp
+# new_filename = f"{name}_{timestamp}{ext}"
+# new_path = os.path.join(folder, new_filename)
+# path_buf = create_string_buffer(new_path.encode('ascii'))  # char path[] (array), NUL added automatically
 
 
 #----- CALIBRATION PROCEDURE -----
@@ -45,76 +52,142 @@ path_buf = create_string_buffer(new_path.encode('ascii'))  # char path[] (array)
 # print("Saved calibration to %s with error code %d" % (new_path, error))
 
 # ------ > LOAD
-error=OB1_Calib_Load (Instr_ID.value, path_buf)
-
-# calibrationDataDouble = (c_double*1000)() #array of 1000 doubles
-# Path(new_path).parent.mkdir(parents=True, exist_ok=True)
-# with open(new_path, "w", encoding="utf-8", newline="\n") as f:
-#     json.dump(calibrationDataDouble, f, ensure_ascii=False)  # ensure_ascii=False keeps non-ASCII if ever needed
-
-# with open(new_path, "rt") as f:
-#     calibrationData = json.load(f)
-# if len(calibrationData) != 1000:
-#     raise ValueError("Calibrationdata in file is misformed: should be list of 1000 elements, not {0} elements".format(len(calibrationData)))
-
-# calibrationDataDouble = (c_double*len(calibrationData))(*calibrationData) #needed for PID control functions
-
-#for i in range(len(calibrationData)):
-#    calibrationDataDouble[i] = c_double(calibrationData[i])
-
-
-#pressure sweep - MFS can only detect flow up to 250 mbar with the current simple setup, ca
-# for i in range(-280, 0):
-#     error = OB1_Set_Press(Instr_ID.value, channel_MFS, c_double(i))
-#     #noticed a bit of offset from the value set here and the pressure value displayed on OB1
-#     #eg. i =278 -> display = 222 mbar  //  i = 309 -> display = 252 mbar
-#     time.sleep(0.1)  # Add a short delay to allow pressure to stabilize
-
+# path_buf = "C:\\Users\\Public\\Documents\\Elvesys\\ESI\\bin\\48V444400113433.calib"
+error=OB1_Calib_Load (Instr_ID.value, Calib_path.encode('ascii'))
 error = OB1_Set_Press(Instr_ID.value, channel_MFS, c_double(0)) #reset the pressure to 0 at the end of the sweep
 
 
-# ---- PID CONTROL ---
-K_p = 0.09
-K_i = 0.003
-target_flow = 300 #ul/min,
+# # ---- BASIC PID CONTROL ---
+# target_flow = -50  # µL/min --> THIS SETS THE PRESSURE TO BE -50 NOT THE FLOWRATE
+# K_p = 0.001
+# K_i = 0.001 # testerd higher P and I values - they are too noisy.
 
-error = PID_Add_Remote(Instr_ID.value, channel_MFS, Instr_ID.value, channel_MFS, K_p, K_i, 1) 
+# # --- INITIALIZE REMOTE PID ---
+# err = PID_Add_Remote(Instr_ID.value, channel_MFS, Instr_ID.value, channel_MFS, K_p, K_i, 1)
 
-error = PID_Set_Running_Remote(Instr_ID.value, channel_MFS, c_int32(1)) #start PID control
-error = OB1_Set_Sens(Instr_ID.value, channel_MFS, c_double(target_flow)) #set target flow rate
-error = PID_Set_Params_Remote(Instr_ID.value, channel_MFS, 1, K_p, K_i) #update PID params if needed
+# err = PID_Set_Running_Remote(Instr_ID.value, channel_MFS, c_int32(1))  # start PID
+# err = OB1_Set_Sens(Instr_ID.value, channel_MFS, c_double(target_flow))  # target flow
+# err = PID_Set_Params_Remote(Instr_ID.value, channel_MFS, 1, K_p, K_i)   # initial PID params
 
-
-# --- READ SENSOR DATA ---
-sensor_readings = (c_double * 50)()
-regulator_readings = (c_double * 50)()
-
-for i in range(50):
-    sen = c_double()
-    reg = c_double()
-    err = OB1_Get_Data(
-        Instr_ID.value,
-        channel_MFS,
-        byref(reg),
-        byref(sen),
-    )
-    sensor_readings[i] = sen.value
-    regulator_readings[i] = reg.value
-
-# Convert c_double array to Python list
-print("Sensor readings (ul/min):", [sensor_readings[i] for i in range(50)])
-print("Regulator readings (mbar):", [regulator_readings[i] for i in range(50)])
-
-#TODO: Try a loop to adjust the PI parameters based on the flow recorded, and a better way of recording the readings over a longer period of time
-#TODO: once these two are confirmed, add the logic to the MUX code.
+# #RESET PRESSURE
+# error = OB1_Set_Press(Instr_ID.value, channel_MFS, c_double(0)) #reset the pressure to 0 at the end of the sweep
 
 
-error = PID_Set_Running_Remote(Instr_ID.value, channel_MFS, c_int32(0)) #stop PID control
-error = OB1_Destructor(Instr_ID.value)
+# --- SQUARE WAVEFORM PID FLOW CONTROL ---
+
+flow_low = -50.0   # µL/min
+flow_high = 50.0  # µL/min
+period_s = 30.0     # full square-wave period (sec)
+n_cycles = 5       # number of cycles to run
+sample_dt = 0.3    # sample interval (sec)
+
+K_p = 0.001
+K_i = 0.001
+err = PID_Add_Remote(Instr_ID.value, channel_MFS,
+                     Instr_ID.value, channel_MFS,
+                     K_p, K_i, 1)
+
+err = PID_Set_Running_Remote(Instr_ID.value, channel_MFS, c_int32(1))  # start PID
+err = PID_Set_Params_Remote(Instr_ID.value, channel_MFS, 1, K_p, K_i)  # load gains
+
+# --- BUFFERS ---
+time_log = []
+target_log = []
+flow_log = []
+regulator_log = []
+
+# --- SQUARE-WAVE EXECUTION ---
+try:
+    n_samples = int(n_cycles * period_s / sample_dt)
+    half_period = period_s / 2.0
+
+    t0 = time.time()
+    for i in range(n_samples):
+        t = time.time() - t0
+
+        # decide square-wave setpoint
+        cycle_time = t % period_s
+        if cycle_time < half_period:
+            target_flow = flow_low
+        else:
+            target_flow = flow_high
+
+        # update setpoint on OB1
+        err = OB1_Set_Sens(Instr_ID.value, channel_MFS, c_double(target_flow))
+
+        # read data
+        sen = c_double()
+        reg = c_double()
+        _ = OB1_Get_Data(Instr_ID.value, channel_MFS, byref(reg), byref(sen))
+
+        # log
+        time_log.append(t)
+        target_log.append(target_flow)
+        flow_log.append(sen.value)
+        regulator_log.append(reg.value)
+
+        time.sleep(sample_dt)
+
+finally:
+    # always reset and stop PID safely
+    _ = OB1_Set_Press(Instr_ID.value, channel_MFS, c_double(0))
+    _ = PID_Set_Running_Remote(Instr_ID.value, channel_MFS, c_int32(0))
+
+# --- ANALYZE + PLOT ---
+df = pd.DataFrame({
+    "time_s": time_log,
+    "target_flow": target_log,
+    "sensor_flow": flow_log,
+    "regulator_mbar": regulator_log,
+})
+
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+csv_path = f"square_flow_log_{timestamp}.csv"
+plot_path = f"square_flow_plot_{timestamp}.png"
+
+df.to_csv(csv_path, index=False)
+print(f"Data saved to {csv_path}")
+
+plt.figure()
+plt.plot(df["time_s"], df["sensor_flow"], label="Measured flow")
+plt.plot(df["time_s"], df["target_flow"], "--", label="Target flow")
+plt.xlabel("Time [s]")
+plt.ylabel("Flow [µL/min]")
+plt.legend()
+plt.title("Square Wave Flow Control")
+plt.savefig(plot_path, dpi=300)
+print(f"Plot saved to {plot_path}")
+
+plt.show()
+
+# # --- READ SENSOR DATA ---
+# sensor_readings = (c_double * 50)()
+# regulator_readings = (c_double * 50)()
+
+# for i in range(50):
+#     sen = c_double()
+#     reg = c_double()
+#     err = OB1_Get_Data(
+#         Instr_ID.value,
+#         channel_MFS,
+#         byref(reg),
+#         byref(sen),
+#     )
+#     sensor_readings[i] = sen.value
+#     regulator_readings[i] = reg.value
+
+# # Convert c_double array to Python list
+# print("Sensor readings (ul/min):", [sensor_readings[i] for i in range(50)])
+# print("Regulator readings (mbar):", [regulator_readings[i] for i in range(50)])
+
+# #TODO: once these two are confirmed, add the logic to the MUX code.
+
+
+# error = PID_Set_Running_Remote(Instr_ID.value, channel_MFS, c_int32(0)) #stop PID control
+# error = OB1_Destructor(Instr_ID.value)
 
 
 
-#-301706 error code is related to the naming of the device and when NI MAX cannot validate the device name. try 
 
 
 
